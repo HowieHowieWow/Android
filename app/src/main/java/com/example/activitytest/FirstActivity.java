ede0487303b315bc772004a4d44402206128c5df
王浩然 <wanghaoran@wanghaorandeMacBook-Pro.local>
2020-12-27 15:57:56 +0800

    track all files

---
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -0,0 +1,90 @@
+package com.example.activitytest;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+
+        return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    Log.d("FirstActivity", data.getStringExtra("data_return"));
+                }
+                break;
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return true;
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+
+        Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //推送Toast
+                //Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+
+                //退回上一层
+                //finish();
+
+                //显式intent
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", "Hello SecondActivity!");
+                //隐式intent
+                //Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                //intent.addCategory("com.example.activitytest.MY_CATEGORY");
+
+                //打开新网页
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent.setData(Uri.parse("https://www.baidu.com"));
+
+                //打开通话界面
+                //Intent intent = new Intent(Intent.ACTION_VIEW);
+                //intent = new Intent(Intent.ACTION_DIAL);
+                //intent.setData(Uri.parse("tel: 10086"));
+
+                //startActivity(intent);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -0,0 +1,42 @@
+package com.example.activitytest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        Intent intent = new Intent();
+        intent.putExtra("data_return", "Comes from SecondActivity BackPressed");
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return", "Comes from SecondActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
+
+    }
+}
